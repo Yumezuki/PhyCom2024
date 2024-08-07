3246f85faef3302e2a9c25f4a3a5a968c9d1b464
Yumezuki <108189514+Yumezuki@users.noreply.github.com>
2024-08-07 10:47:05 +0700

    Tor mak

---
+++ Week6/Lab6-1_Index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num, i = 0;
+
+    int M[] = {2, 20, 8, 10, 4, 6, 16, 18};
+    int N[] = {1, 3, 9, 7, 11, 15, 19};
+
+    printf("")
+    // while (i > 0) {
+    //     scanf("%d", &num);
+    //     if (num >= 1 && num <=20) {
+    //         printf("%d ");
+    //         break;
+    //     }
+    // }
+
+    return 0;
+}
